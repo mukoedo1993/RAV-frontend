8a02cb4622b2422cc84c51e6162e0df091e1df4c
mukoedo1993 <wangzcyuanfang1997@gmail.com>
2021-09-02 21:18:41 -0700

    until course 99th

--- course70th/README.md
+++ course70th/README.md
@@ -4,3 +4,7 @@ create-react-app react-framework-intro
 cd react*
 npm start
 ```
+
+```
+npm install react-router-dom --save # https://stackoverflow.com/questions/53914013/failed-to-compile-module-not-found-cant-resolve-react-router-dom?rq=1
+```
--- course70th/react-framework-intro/src/App.js
+++ course70th/react-framework-intro/src/App.js
@@ -1,25 +1,31 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
+import User from './components/User'
+import Account from './components/Account'
+import Blog from './components/Blog'
 
 function App() {
+
+  //load the component in the place where we define the Router
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <div>
+          <Link to="">Home</Link> |
+          <Link to="/account">Account</Link> |
+          <Link to="/blog">Blog</Link>
+        </div>
+        <hr/>
+      <Route exact path="/" component={User}  /> 
+      <Route path="/account" component={Account}  />
+      <Route path="/blog" component={Blog}  />
+      </div>
+
+    </ Router>
+  ); //exact: so we only need to consider the exact matched path here.
 }
 
 export default App;
